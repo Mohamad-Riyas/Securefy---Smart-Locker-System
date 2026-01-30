87e3c6a91e87839b7c814da171941f67f3e1c424
Mohamad Riyas <mohamadriyasmax54@gmail.com>
2026-01-30 23:56:47 +0530

    Refactor App component and update routes

--- frontend/securefy/src/App.jsx
+++ frontend/securefy/src/App.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 import "./App.css";
 
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./pages/Home";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
+    <>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Auth Pages */}
+        <Route path="/login" element={<AuthLayout><Login /></AuthLayout>} />
+        <Route path="/register" element={<AuthLayout><Register /></AuthLayout>} />
+      </Routes>
+
+      <ToastContainer />
+    </>
+  );
+}
+
+/* Auth Layout */
+function AuthLayout({ children }) {
+  return (
     <div className="App">
       <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-          <ToastContainer />
-        </div>
+        <div className="auth-inner">{children}</div>
       </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
