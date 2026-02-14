e0678b26365f0f36be34bc4240e373eb53df07b6
RajasegaranDhanush <rajasegarandhanush@gmail.com>
2026-02-14 19:57:18 +0530

    Added CSS file

--- frontend/securefy/src/firebase.js
+++ frontend/securefy/src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; // ADDED THIS LINE
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +20,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+// Initialize Cloud Firestore and export it
+export const db = getFirestore(app); // ADDED THIS LINE
+
 export default app;
