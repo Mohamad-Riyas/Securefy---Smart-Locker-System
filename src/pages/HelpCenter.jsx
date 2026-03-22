d363654382506258ce538323da886499430f044a
AqeelAroos <aqeelaroos730@gmail.com>
2026-03-22 10:40:46 +0530

    Update HelpCenter.jsx  simplify component structure

--- src/pages/HelpCenter.jsx
+++ src/pages/HelpCenter.jsx
@@ -4,21 +4,7 @@ import { FaEnvelope, FaPhoneAlt, FaQuestionCircle, FaBook } from 'react-icons/fa
 
 
 
-const fadeUp = {
-  hidden: { opacity: 0, y: 30 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
-};
-
-
-
-const staggerContainer = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { staggerChildren: 0.1 } }
-};
-
-
-
-export default function HelpCenter() {
+const HelpCenter = () => {
   return (
 
     // background styling from the Home page
